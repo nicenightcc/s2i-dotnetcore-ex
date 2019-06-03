0abe4534f0eccecf7f1594eb0d699545d6a7c87a
nicenight <nicenight@nicenight.cc>
2019-06-03 15:48:43 +0800

    proxy test

--- app/Program.cs
+++ app/Program.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace app
 {
@@ -7,7 +10,27 @@ namespace app
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");
-      new SocketConnection(8080).setConnection();
+
+      const int port = 8080;
+      //定义端口号
+      TcpListener tcplistener = new TcpListener(IPAddress.Any, port);
+      Console.WriteLine("侦听端口号： " + port.ToString());
+      tcplistener.Start();
+      //侦听端口号
+      while (true)
+      {
+        Socket socket = tcplistener.AcceptSocket();
+        //并获取传送和接收数据的Scoket实例
+        Proxy proxy = new Proxy(socket);
+        //Proxy类实例化
+        Thread thread = new Thread(new ThreadStart(proxy.Run));
+        //创建线程
+        thread.Start();
+        //启动线程
+      }
+
+
+      //new SocketConnection(8080).setConnection();
       while (true)
         Console.Read();
     }
